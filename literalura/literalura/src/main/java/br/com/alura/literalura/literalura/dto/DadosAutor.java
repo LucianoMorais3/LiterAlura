9fa809c6eb00cda0aa2aa495f6746009676cdebd
LucianoMorais3 <lucianosantosmorais3@gmail.com>
2025-06-30 12:04:48 -0300

    Adiciona funcionalidade de busca de livros e exibição dos resultados

---
+++ literalura/literalura/src/main/java/br/com/alura/literalura/literalura/dto/DadosAutor.java
@@ -0,0 +1,4 @@
+package br.com.alura.literalura.literalura.dto;
+
+public record DadosAutor() {
+}
---
+++ literalura/literalura/src/main/java/br/com/alura/literalura/literalura/dto/RespostaApi.java
@@ -0,0 +1,10 @@
+package br.com.alura.literalura.literalura.model;
+
+
+import br.com.alura.literalura.literalura.dto.DadosLivro;
+
+import java.util.List;
+
+public record RespostaApi(List<DadosLivro> results) {
+
+}
